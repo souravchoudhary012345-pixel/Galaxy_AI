e8c56e765f6129c77bde4fc2ce4adcb0743aa820
Sourav Choudhary <souravhr94@gmail.com>
2026-01-13 08:57:01 +0530

    chore: project scaffolding and configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+
+const config: Config = {
+darkMode: "class",
+content: [
+"./app/**/*.{ts,tsx}",
+"./components/**/*.{ts,tsx}",
+],
+plugins: []
+};
+
+
+export default config;
